1aadc9ddabbfe6e323a97dad521b0030d2577bef
Allan Zhao <allanzhao@users.noreply.github.com>
2025-06-17 03:47:17 -0400

    Support external high-speed USB PHY

--- src/otg_hs.rs
+++ src/otg_hs.rs
@@ -2,9 +2,6 @@
 //!
 //! Requires the `usb_hs` feature.
 //! Only one of the `usb_fs`/`usb_hs` features can be selected at the same time.
-//!
-//! Note that only full-speed mode is supported,
-//! external high-speed PHY is not supported.
 
 use crate::pac;
 
@@ -71,3 +68,99 @@ unsafe impl UsbPeripheral for USB {
 }
 
 pub type UsbBusType = UsbBus<USB>;
+
+pub struct UsbUlpi {
+    pub usb_global: pac::OTG_HS_GLOBAL,
+    pub usb_device: pac::OTG_HS_DEVICE,
+    pub usb_pwrclk: pac::OTG_HS_PWRCLK,
+    pub ulpi_clk: alt::UlpiCk,
+    pub ulpi_dir: alt::UlpiDir,
+    pub ulpi_nxt: alt::UlpiNxt,
+    pub ulpi_stp: alt::UlpiStp,
+    pub ulpi_d0: alt::UlpiD0,
+    pub ulpi_d1: alt::UlpiD1,
+    pub ulpi_d2: alt::UlpiD2,
+    pub ulpi_d3: alt::UlpiD3,
+    pub ulpi_d4: alt::UlpiD4,
+    pub ulpi_d5: alt::UlpiD5,
+    pub ulpi_d6: alt::UlpiD6,
+    pub ulpi_d7: alt::UlpiD7,
+    pub hclk: Hertz,
+}
+
+impl UsbUlpi {
+    #[allow(clippy::too_many_arguments)]
+    pub fn new(
+        usb_global: pac::OTG_HS_GLOBAL,
+        usb_device: pac::OTG_HS_DEVICE,
+        usb_pwrclk: pac::OTG_HS_PWRCLK,
+        ulpi_clk: impl Into<alt::UlpiCk>,
+        ulpi_dir: impl Into<alt::UlpiDir>,
+        ulpi_nxt: impl Into<alt::UlpiNxt>,
+        ulpi_stp: impl Into<alt::UlpiStp>,
+        ulpi_d0: impl Into<alt::UlpiD0>,
+        ulpi_d1: impl Into<alt::UlpiD1>,
+        ulpi_d2: impl Into<alt::UlpiD2>,
+        ulpi_d3: impl Into<alt::UlpiD3>,
+        ulpi_d4: impl Into<alt::UlpiD4>,
+        ulpi_d5: impl Into<alt::UlpiD5>,
+        ulpi_d6: impl Into<alt::UlpiD6>,
+        ulpi_d7: impl Into<alt::UlpiD7>,
+        clocks: &Clocks,
+    ) -> Self {
+        UsbUlpi {
+            usb_global,
+            usb_device,
+            usb_pwrclk,
+            ulpi_clk: ulpi_clk.into(),
+            ulpi_dir: ulpi_dir.into(),
+            ulpi_nxt: ulpi_nxt.into(),
+            ulpi_stp: ulpi_stp.into(),
+            ulpi_d0: ulpi_d0.into(),
+            ulpi_d1: ulpi_d1.into(),
+            ulpi_d2: ulpi_d2.into(),
+            ulpi_d3: ulpi_d3.into(),
+            ulpi_d4: ulpi_d4.into(),
+            ulpi_d5: ulpi_d5.into(),
+            ulpi_d6: ulpi_d6.into(),
+            ulpi_d7: ulpi_d7.into(),
+            hclk: clocks.hclk(),
+        }
+    }
+}
+
+unsafe impl Sync for UsbUlpi {}
+
+unsafe impl UsbPeripheral for UsbUlpi {
+    const REGISTERS: *const () = pac::OTG_HS_GLOBAL::ptr() as *const ();
+
+    const HIGH_SPEED: bool = true;
+    const FIFO_DEPTH_WORDS: usize = 1024; // RM0090 page 1429
+    const ENDPOINT_COUNT: usize = 6; // RM0090 page 1386
+
+    fn enable() {
+        let rcc = unsafe { &*pac::RCC::ptr() };
+
+        cortex_m::interrupt::free(|_| {
+            // Enable ULPI clock
+            rcc.ahb1enr().modify(|_, w| w.otghsulpien().enabled());
+        });
+
+        unsafe {
+            // Enable USB peripheral
+            pac::OTG_HS_GLOBAL::enable_unchecked();
+            // Reset USB peripheral
+            pac::OTG_HS_GLOBAL::reset_unchecked();
+        }
+    }
+
+    fn ahb_frequency_hz(&self) -> u32 {
+        self.hclk.raw()
+    }
+
+    fn phy_type(&self) -> synopsys_usb_otg::PhyType {
+        synopsys_usb_otg::PhyType::ExternalHighSpeed
+    }
+}
+
+pub type UsbUlpiBusType = UsbBus<UsbUlpi>;
